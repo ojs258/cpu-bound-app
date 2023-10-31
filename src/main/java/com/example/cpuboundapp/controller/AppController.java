617674232dcd00baafd0f7195eb7179d65560495
oh jinseok <ojs258@naver.com>
2023-10-31 11:09:54 +0900

    test

--- src/main/java/com/example/cpuboundapp/controller/AppController.java
+++ src/main/java/com/example/cpuboundapp/controller/AppController.java
@@ -20,7 +20,7 @@ public class AppController {
 
     @RequestMapping("/hello")
     public String hello() {
-        return "hello ci/cd test rolling deploy test-->";
+        return "hello ci/cd test rolling deploy test";
     }
 
     private String getMD5Digest(String input) throws NoSuchAlgorithmException {
