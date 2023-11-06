eb227cc851dc22db96c0abefbd5db19514b88a2e
oh jinseok <ojs258@naver.com>
2023-11-06 21:28:52 +0900

    1365151

--- src/main/java/com/example/cpuboundapp/controller/AppController.java
+++ src/main/java/com/example/cpuboundapp/controller/AppController.java
@@ -20,7 +20,7 @@ public class AppController {
 
     @RequestMapping("/hello")
     public String hello() {
-        return "Git Hook deploy 2";
+        return "Git Hook deploy 3";
     }
 
     private String getMD5Digest(String input) throws NoSuchAlgorithmException {
